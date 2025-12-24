39bbdc0d370040e3122f008a9e7866227a9078da
Vaishnav <chandurkarvaishnav@gmail.com>
2025-12-24 14:44:33 +0530

    feat: login api

--- apps/server/src/domains/auth/auth.routes.ts
+++ apps/server/src/domains/auth/auth.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import asyncHandler from "../../lib/async-handler";
 import error_messages from "../../lib/errors-messages";
-import { isValidEmail } from "../../lib/utils";
+import { isValidEmail, verifyPasswordHash } from "../../lib/utils";
 import UserService from "../user/user.service";
 import CustomError from "../../lib/custom-error";
 
@@ -43,7 +43,46 @@ authRouter.post(
 
     UserService.setSessionHeaders(res, sessionToken);
 
-    res.sendSuccess({ user });
+    res.sendSuccess({ user: UserService.toDTO(user) });
+  })
+);
+
+authRouter.post(
+  "/login",
+  asyncHandler(async (req, res) => {
+    const data = req.body;
+
+    if (!data.email?.trim().length) {
+      return res.sendError(error_messages.auth.empty_email);
+    }
+
+    if (!data.password?.trim().length) {
+      return res.sendError(error_messages.auth.empty_password);
+    }
+
+    const user = await UserService.getByEmail(data.email);
+
+    if (!user) {
+      throw new CustomError(error_messages.auth.invalid_email_or_password);
+    }
+
+    const isPasswordValid = await verifyPasswordHash(
+      user.password,
+      data.password
+    );
+
+    if (!isPasswordValid) {
+      throw new CustomError(error_messages.auth.invalid_email_or_password);
+    }
+
+    const sessionToken = await UserService.createSession({
+      userId: user.id,
+      userAgent: req.headers["user-agent"] || "unknown",
+    });
+
+    UserService.setSessionHeaders(res, sessionToken);
+
+    res.sendSuccess({ user: UserService.toDTO(user) });
   })
 );
 
