657fcc2b377bc4d2440070fccae58fae7fbd9378
Vaishnav <chandurkarvaishnav@gmail.com>
2025-12-28 12:10:43 +0530

    chores: remove use client

--- apps/web/components/ui/separator.tsx
+++ apps/web/components/ui/separator.tsx
@@ -1,9 +1,7 @@
-"use client"
+import * as React from "react";
+import { Separator as SeparatorPrimitive } from "radix-ui";
 
-import * as React from "react"
-import { Separator as SeparatorPrimitive } from "radix-ui"
-
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 function Separator({
   className,
@@ -22,7 +20,7 @@ function Separator({
       )}
       {...props}
     />
-  )
+  );
 }
 
-export { Separator }
+export { Separator };
