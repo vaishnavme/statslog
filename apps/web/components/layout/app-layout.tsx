bae16aeb65bcc78bf8f2be1850c780bcae1a96da
Vaishnav <chandurkarvaishnav@gmail.com>
2025-12-30 11:41:12 +0530

    improve: hide side panel in logout mode

--- apps/web/components/layout/app-layout.tsx
+++ apps/web/components/layout/app-layout.tsx
@@ -1,3 +1,4 @@
+import useUserStore from "@/store/user-store";
 import BottomNavbar from "./bottom-navbar";
 import SidePanel from "./side-panel";
 
@@ -8,10 +9,14 @@ interface AppLayoutProps {
 const AppLayout = (props: AppLayoutProps) => {
   const { children } = props;
 
+  const user = useUserStore((state) => state.user);
+
   return (
     <div className="relative max-w-6xl mx-auto">
       <SidePanel />
-      <main className="md:ml-56 min-h-screen">{children}</main>
+      <main className={`${user?.id ? "ml-56" : ""} min-h-screen`}>
+        {children}
+      </main>
       <BottomNavbar />
     </div>
   );
